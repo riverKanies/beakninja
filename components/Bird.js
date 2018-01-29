bc424263c0345dd9e4b33c80c6bc453972373f03
River Kanies <river.kanies@gmail.com>
2018-01-29 14:13:52 -0500

    move l/r y offset

--- components/Bird.js
+++ components/Bird.js
@@ -1,5 +1,6 @@
 const tileSize = 30
 export default (props) => {
+    const {moving, frame} = props.bird
     const dir = props.bird.dir || 'down'
     let x = 0
     let y = 0
@@ -9,14 +10,16 @@ export default (props) => {
         x=-10
         y=-20
     } else if (dir == 'left') {
-        y=-20
+        y=-10
+        if (!moving) y=-20
     } else if (dir == 'right') {
-        y=-20
+        y=-10
+        if (!moving) y=-20
         x=-20
     }
     const transform = `translate(${x},${y})`
-    let birdFrame = props.bird.frame || 1
-    if (props.bird.moving) birdFrame = 1
-    const birdImage = `/static/bird/${(props.bird.moving ? 'move-' : '')}${dir}${birdFrame}.PNG`
+    let birdFrame = frame || 1
+    if (moving) birdFrame = 1
+    const birdImage = `/static/bird/${(moving ? 'move-' : '')}${dir}${birdFrame}.PNG`
     return <image x={props.bird.x*tileSize} y={props.bird.y*tileSize} href={birdImage} transform={transform}/>
 }
