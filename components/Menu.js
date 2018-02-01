ed83a9c6a3e5f839d6f0efd572d4d57bf06dd222
River Kanies <river.kanies@gmail.com>
2018-02-01 12:30:26 -0500

    menu styles

--- components/Menu.js
+++ components/Menu.js
@@ -1,9 +1,23 @@
+const space = 180
+const levels = [1, 2, 3]
 export default (props) => {
     const vb = props.vb
     return props.open ? (
         <g>
             <rect x={vb[0]} y={vb[1]} width={vb[2]} height={vb[3]} fill={'white'} opacity={'.5'}/>
-            <text x={vb[2]/2} y={vb[3]/2} fill={'black'} textAnchor={'middle'} fontSize={'50px'} onMouseDown={props.startLevel(1)}>Level 1</text>
+            {levels.map((num)=>{
+                return <text key={num}
+                    x={(vb[2]/2) - (space*(levels.length-1)/2) + (space*(num-1))}
+                    y={vb[3]/2 + 30}
+                    fill={'hsl(280,80%,40%)'}
+                    stroke={'hsl(280,70%,60%)'}
+                    textAnchor={'middle'}
+                    fontSize={'150px'}
+                    strokeWidth={2}
+                    onMouseDown={props.startLevel(num)}>
+                        {num}
+                </text>
+            })}
         </g>
     ) :''
 }
