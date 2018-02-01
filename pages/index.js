13169882e0e557265c9772b1e9d4e6df01cab8e1
River Kanies <river.kanies@gmail.com>
2018-02-01 07:24:28 -0500

    body styles and crisp pixel images

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,18 @@ import Game from '../components/Game'
 
 const Index = () => (
     <div>
-        <Header />
         <Game />
+        <style jsx global>{`
+          body {
+              image-rendering: -moz-crisp-edges;
+              image-rendering: -webkit-crisp-edges;
+              image-rendering: pixelated;
+              image-rendering: crisp-edges;
+          
+              background: darkgray;
+              margin: 0 5%;
+          }
+      `}</style>
     </div>
 )
 
